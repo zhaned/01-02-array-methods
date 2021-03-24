014689637abaa9f5fa1abc8dbaad3e382269bcf8
zhaned <zhanedmond@gmail.com>
2021-03-23 23:03:57 -0700

    set up functions and test files

---
+++ index.js
@@ -0,0 +1,27 @@
+const maps = () => {
+
+};
+
+const filters = () => {
+
+};
+
+const findIndexes = () => {
+
+};
+
+const reduces = () => {
+
+};
+
+const everys = () => {
+
+};
+
+module.exports = {
+  maps,
+  filters,
+  findIndexes,
+  reduces,
+  everys
+};
---
+++ index.test.js
@@ -0,0 +1,16 @@
+const {
+  maps,
+  filters,
+  findIndexes,
+  reduces,
+  everys
+} = require('./index');
+
+describe('tests functions similar to array operations', () => {
+  it('', () => {
+    
+
+    expect().toEqual();
+  });
+
+});
